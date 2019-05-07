2e8bd191aac1383cb85015de8ec14e6afa0e8f48
Lor04 <46799119+Lor04@users.noreply.github.com>
2019-05-07 16:58:02 -0600

    Final Exam

---
+++ FinalExam-master/Assets/Next.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // need to change scenes
+
+public class Next : MonoBehaviour
+{
+    public void StartGame() //starts the game and scene transactions
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void BackSceneGame() //starts the game and scene transactions
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ FinalExam-master/Assets/ScoreKeeper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    static public int newScore = 0;
+    public Text NewScore;
+    public Text HighScore;
+
+
+    void Start()
+    {
+        HighScore.text = PlayerPrefs.GetInt("HighScore",0).ToString();
+    }
+
+    public void IncreaseScore() // clicking on + button increases score
+    {
+        newScore += 1;
+        NewScore.text = newScore.ToString();
+        Debug.Log(newScore);
+        PlayerPrefs.SetInt("HighScore", newScore);
+    }
+
+    public void DecreaseScore(int) // clicking on - button decreases score
+    {
+        newScore -= 1;
+        NewScore.text = newScore.ToString();
+        Debug.Log(newScore);
+        PlayerPrefs.SetInt("Highscore", newScore);
+
+    }
+    
+}
